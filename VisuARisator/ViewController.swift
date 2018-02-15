0e36b2a7bbb1c3589bd750fe1b7bfd4262088ac0
TheGreatNectarine <31987375+TheGreatNectarine@users.noreply.github.com>
2018-02-15 13:20:23 +0200

    first

---
+++ VisuARisator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisuARisator
+//
+//  Created by Nick Marhal on 2/15/18.
+//  Copyright © 2018 LKNM Production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
